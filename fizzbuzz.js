267b65cf2b770c447c21e29c8abe2b2437ed05f3
Joey Gold <joey.gold@softwire.com>
2025-01-20 15:06:22 +0000

    print hello 100 times

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -5,6 +5,9 @@ function fizzbuzz() {
     
     console.log('Welcome to FizzBuzz!');
     console.log('Hello world. ');
+    for (let i = 0; i < 100; i++) {
+        console.log('hello');
+    }
     // Put your code here...
 
     
