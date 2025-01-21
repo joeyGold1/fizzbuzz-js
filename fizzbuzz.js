679899ea7e865c53fbe528c736ebf35f1c5222d9
Joey Gold <joey.gold@softwire.com>
2025-01-21 10:13:32 +0000

    Add buzz rule

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -8,8 +8,12 @@ function fizzbuzz() {
 }
 
 function getFizzBuzzOutput(i) {
-    if (i % 3 == 0) {
-        return 'fizz';
+    if (i % 3 == 0 && i % 5 == 0) {
+        return 'FizzBuzz'
+    } else if (i % 3 == 0) {
+        return 'Fizz';
+    } else if (i % 5 == 0) {
+        return 'Buzz';
     }
     return i;
 }
